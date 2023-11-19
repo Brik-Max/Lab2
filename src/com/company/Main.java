3aebd384280f2430baea0a23db0043a5b70da096
Mr.BrickCH@outlook.com <Mr.BrickCH@outlook.com>
2023-11-19 16:51:11 +0700

    321

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 
 public class Main {
     public static void main(String[] args) {
+
         JFrame frame = new JFrame("Стоимость чая");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,200);
